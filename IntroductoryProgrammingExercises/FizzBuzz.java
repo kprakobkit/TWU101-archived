3fa1b9fb97268c9ab5a3d83d80504a870bc84991
Peter Prakobkit <kprakobkit@gmail.com>
2015-04-08 06:37:49 +0700

    Completed FizzBuzz exercise.

--- IntroductoryProgrammingExercises/FizzBuzz.java
+++ IntroductoryProgrammingExercises/FizzBuzz.java
@@ -9,6 +9,14 @@ public class FizzBuzz {
     }
 
     public static void FizzBuzz() {
+        for (int i = 1; i <= 100; i++) {
+            String output = "";
 
+            if (i % 3 == 0) output += "Fizz";
+            if (i % 5 == 0) output += "Buzz";
+            if (output.length() == 0) output += i;
+
+            System.out.println(output);
+        }
     }
 }
