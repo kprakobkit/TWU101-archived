fa2f82cda337eef96ddf9d27bed34fe5636facc3
Peter Prakobkit <kprakobkit@gmail.com>
2015-04-08 06:31:56 +0700

    Added FizzBuzz class

---
+++ IntroductoryProgrammingExercises/FizzBuzz.java
@@ -0,0 +1,14 @@
+package IntroductoryProgrammingExercises;
+
+/**
+ * Created by kprakobkit on 4/8/15.
+ */
+public class FizzBuzz {
+    public static void main(String[] args) {
+        FizzBuzz();
+    }
+
+    public static void FizzBuzz() {
+
+    }
+}
