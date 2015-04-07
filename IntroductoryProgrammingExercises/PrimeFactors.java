4b91c8745e2eb9e9dc00e1316c7e4b44238b726c
Peter Prakobkit <kprakobkit@gmail.com>
2015-04-08 06:42:41 +0700

    Add PrimeFactors class

---
+++ IntroductoryProgrammingExercises/PrimeFactors.java
@@ -0,0 +1,14 @@
+package IntroductoryProgrammingExercises;
+
+/**
+ * Created by kprakobkit on 4/8/15.
+ */
+public class PrimeFactors {
+    public static void main(String[] args) {
+        printPrimeFactors(30);
+    }
+
+    public static void printPrimeFactors(int n) {
+
+    }
+}
