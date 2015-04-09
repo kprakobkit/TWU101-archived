10f6accef0b86ad79814d1f389a2ff23e23ad6e5
Peter Prakobkit <kprakobkit@gmail.com>
2015-04-09 09:58:08 +0700

    Added GuessingGame package. Added GuessingGame class. Added GuessingGameTestDrive class.

---
+++ Chapter5/GuessingGame.java
@@ -0,0 +1,8 @@
+package Chapter5;
+
+/**
+ * Write a program that chooses a random number between 1 and 100. Then asks the user to enter a guess. If they guess right then tell them they win, otherwise tell them if they guessed too high or low. Keep asking the user to guess until they get the right answer. Use classes to separate the different concerns of this program.
+ * Created by kprakobkit on 4/9/15.
+ */
+public class GuessingGame {
+}
---
+++ Chapter5/GuessingGameTestDrive.java
@@ -0,0 +1,7 @@
+package Chapter5;
+
+/**
+ * Created by kprakobkit on 4/9/15.
+ */
+public class GuessingGameTestDrive {
+}
