f3a4f14682d0b25e581e23d3484ce1c34fa80d20
Peter Prakobkit <kprakobkit@gmail.com>
2015-04-10 09:14:55 +0700

    Remove scanner library Add Chapter6 Guessing Game package and class

--- Chapter5/GuessingGame.java
+++ Chapter5/GuessingGame.java
@@ -1,7 +1,6 @@
 package Chapter5;
 
 import java.util.Random;
-import java.util.Scanner;
 
 /**
  * Write a program that chooses a random number between 1 and 100. Then asks the user to enter a guess. If they guess right then tell them they win, otherwise tell them if they guessed too high or low. Keep asking the user to guess until they get the right answer. Use classes to separate the different concerns of this program.
---
+++ Chapter6/GuessingGameTestDrive.java
@@ -0,0 +1,7 @@
+package Chapter6;
+
+/**
+ * Created by kprakobkit on 4/10/15.
+ */
+public class GuessingGameTestDrive {
+}
