df854cc4fa94693c8da8cb54df2a8355a0647836
Peter Prakobkit <kprakobkit@gmail.com>
2015-04-10 09:47:32 +0700

    Completed exercise - Chapter 6 Guessing Game version 2

--- Chapter6/GuessingGameTestDrive.java
+++ Chapter6/GuessingGameTestDrive.java
@@ -4,4 +4,8 @@ package Chapter6;
  * Created by kprakobkit on 4/10/15.
  */
 public class GuessingGameTestDrive {
+    public static void main(String[] args) {
+        GuessingGame guessingGame = new GuessingGame();
+        guessingGame.start();
+    }
 }
